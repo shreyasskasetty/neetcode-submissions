27e54e441ba18463a6c8ff6bb6eca5db2c3f50fa
shreyasskasetty <35930455+shreyasskasetty@users.noreply.github.com>
2026-06-14 22:07:00 -0700

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        reducedS = ""
+        for x in s:
+            if x.isalnum():
+                reducedS += x.lower()
+        print(reducedS)
+        print(len(reducedS))
+        for i in range(len(reducedS)//2):
+            if reducedS[i] != reducedS[len(reducedS) - i - 1]:
+                return False
+        return True
