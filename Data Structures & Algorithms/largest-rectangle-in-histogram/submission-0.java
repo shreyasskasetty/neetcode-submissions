e8cf0180886078039e4e4326199cec04d996ecb8
shreyasskasetty <35930455+shreyasskasetty@users.noreply.github.com>
2026-07-08 17:29:05 -0700

    Add: largest-rectangle-in-histogram - submission-0

---
+++ Data Structures & Algorithms/largest-rectangle-in-histogram/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int maxArea = Integer.MIN_VALUE;
+        for(int i = 0; i < heights.length; i++){
+            int minHeight = Integer.MAX_VALUE;
+            for(int j = i; j < heights.length; j++){
+                minHeight = Math.min(minHeight, heights[j]);
+                maxArea = Math.max(maxArea, minHeight * (j - i + 1));
+            }
+            
+        }
+        return maxArea;
+    }
+}
